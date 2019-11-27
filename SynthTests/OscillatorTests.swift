5997347124a9f7b3d40ba35c7e1c23d747be8ee8
Alexey Naumov <a.naumov91@gmail.com>
2019-11-27 21:26:36 +0300

    Add test target

---
+++ SynthTests/OscillatorTests.swift
@@ -0,0 +1,13 @@
+//
+//  Oscillator.swift
+//  Oscillator
+//
+//  Created by Alexey Naumov on 27.11.2019.
+//  Copyright © 2019 Grant Emerson. All rights reserved.
+//
+
+import XCTest
+@testable import Swift_Synth
+
+class OscillatorTests: XCTestCase {
+}
